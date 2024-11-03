413d3bb25c7258ad8b0272eefa6dd9acc9e0d38e
Lilia <liliaabracos124@aeffl.pt>
2024-11-03 16:31:41 +0000

    Adicionei estados

--- main.py
+++ main.py
@@ -2,6 +2,13 @@
 import pygame
 from random import randint, randrange
 
+# Adicionei algumas coisas em casa ontem, para adiantar algum trabalho
+
+
+# TODO:
+# Reiniciar o jogo cada ronda(quando todos os patos sairem do ecra)
+# Dificuldade
+
 score = 0
 patosm = 0
 
@@ -15,8 +22,7 @@ my_font = pygame.font.SysFont('Comic Sans MS', 30)
 
 # Define objects
 class Object:
-    def __init__(self, image, x, y, width, height, frameTime):
-
+    def __init__(self, image, x, y, width, height, frameTime, id):
         self.x = x
         self.y = y
         self.image = image
@@ -27,9 +33,24 @@ class Object:
         self.curFrame = 0
         self.vel = 0
         self.frameTime = frameTime
+        self.id = id
+        # Agora cada objeto tem estados, só é usado nos patos
+        self.state = 0
 
     def draw(self, background):
-        background.blit(self.image[self.curFrame], (self.x, self.y))
+        # Isto foi necessario para fazer funcionar, NAO TIRAR
+        try:
+            screen.blit(self.image[self.state][self.curFrame//self.frameTime],  (self.x, self.y))
+        except:
+            print(self.curFrame // self.frameTime)
+            print(len(self.image[self.state]))
+
+        self.curFrame += 1
+        if self.curFrame // self.frameTime >= len(self.image[self.state]):
+            self.curFrame = 0
+
+    def changeState(self, state):
+        self.state = state
 
 
 class sprite:
@@ -40,13 +61,17 @@ class sprite:
         self.image = []
     
     def load(self, path):
+        tempList = []
         for i in path:
-            self.temp = pygame.image.load(i)
-            self.temp = pygame.transform.scale(self.temp, (self.width, self.height))
-            self.image.append(self.temp)
+            temp = pygame.image.load(i)
+            temp = pygame.transform.scale(temp, (self.width, self.height))
+            tempList.append(temp)
+
+        self.image.append(tempList)
 
 duck = sprite(100, 100)
 duck.load(["duck1.png", "duck2.png", "duck3.png"])
+duck.load(["duckHit.png"])
 
 cursor = sprite(100, 100)
 cursor.load(["cursor.png"])
@@ -64,7 +89,7 @@ def spawnDuckie():
 
     while val == False:
         val = True
-        temp = Object(duck.image, 0, 0, duck.width, duck.height, 10)
+        temp = Object(duck.image, 0, 0, duck.width, duck.height, 10, "duck")
 
         temp.y = screen.get_height() / randint(1, 20)
 
@@ -73,7 +98,7 @@ def spawnDuckie():
                 val = False
         
     #temp.y = randint(1, 500)
-    print(temp.y)
+    #print(temp.y)
     scene.append(temp)
 
 for i in range(0, 5):
@@ -86,57 +111,59 @@ def events():
 def checkMouse():
     mousePos = pygame.mouse.get_pos()
     global score
+    global patosm
     #print(mousePos)
     for i in scene:
-        if i.alive:
-            if mousePos[0] > i.x and mousePos[0] < i.x + i.width:
-                if mousePos[1] > i.y and mousePos[1] < i.y + i.height:
-                    i.alive = False
-                    score += 1
-                    patosm = patosm + 1
+        if i.id == "duck":
+            if i.alive:
+                if mousePos[0] > i.x and mousePos[0] < i.x + i.width:
+                    if mousePos[1] > i.y and mousePos[1] < i.y + i.height:
+                        i.changeState(1)
+                        #i.alive = False
+                        score += 1
+                        patosm = patosm + 1
 conta = 0
 
 # Render function
 def render(count):
+    global patosm
     events()
     text = my_font.render("score: " + str(score), False, (0, 0, 0))
     for i in scene:
-        if i.conta < 50:
-            if patosm > 0:
-                i.x += randint(50, 80)
-                i.y += randint (20, 40)
-            i.x += 2
-            i.y += 1
+        if i.state == 0:
+            if i.conta < 50:
+                if patosm > 0:
+                    i.x += randint(50, 80)
+                    i.y += randint (20, 40)
+                i.x += 2
+                i.y += 1
+            else:
+                if patosm > 0:
+                    i.x += randint(50, 80)
+                    i.y += randint (20, 40)
+                i.x += 2
+                i.y -= 1
+            if i.conta == 100:
+                #print(i.conta)
+                i.conta = 0
+            i.conta += 1
         else:
-            if patosm > 0:
-                i.x += randint(50, 80)
-                i.y += randint (20, 40)
-            i.x += 2
-            i.y -= 1
-        if i.conta == 100:
-            print(i.conta)
-            i.conta = 0
-        i.conta += 1
-    
-
+            i.y += 6
     
     screen.fill((0, 150, 255))
-    screen.blit(background.image[0], (0, 0))
     for i in scene:
         if i.alive == True:
-            screen.blit(i.image[i.curFrame//i.frameTime],  (i.x, i.y))
-            #screen.blit(i.image,  (i.x, i.y))
-            i.curFrame += 1
-            if i.curFrame // i.frameTime >= len(i.image):
-                i.curFrame = 0
+            i.draw(screen)
+
+    screen.blit(background.image[0][0], (0, 0))
 
     # flip() the display to put your work on screen
     screen.blit(text, (3, 3))
     temp = (pygame.mouse.get_pos()[0] - cursor.width/2, pygame.mouse.get_pos()[1] - cursor.height/2)
-    screen.blit(cursor.image[0], temp)
+    screen.blit(cursor.image[0][0], temp)
     pygame.display.flip()
 
-    conta 
+    #conta 
 
 # Ralsei é o melhor (não, patos são, QUACKIESSSS :3)
 #imagina ter comentarios >:(
